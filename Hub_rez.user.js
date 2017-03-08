4deb9eb4bae44e07fc86f1ec79121f721520b82b
MartR <juneyourtech@users.noreply.github.com>
2017-03-09 00:38:42 +0200

    Add files via upload
    Uploaded files.

---
+++ Hub_rez.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Hub Rez
+// @namespace   hub_rez
+// @description Changes GitHub appearance for mobile. All trademarks are the property of their respective owners.
+// @include     https://github.com/*
+// @version     1
+// @grant       GM_addStyle
+// ==/UserScript==
+
+/*
+@updateURL https://github.com/juneyourtech/GM_GitHub/raw/master/wikipedia_vector.user.js */
+
+GM_addStyle("HTML > BODY {min-width:320px;}") //
+
+GM_addStyle("DIV.container {width:auto; max-width:980px;}") //
+
+GM_addStyle("UL.user-nav {margin-right:0px;}") //
+
+GM_addStyle("DIV.file-wrap > TABLE.file TD.message .css-truncate {max-width:unset;}") //
+
+//GM_addStyle("@media screen {A:visited {color:MediumPurple; text-decoration:underline; background:linear-gradient(to right, yellow, transparent 50%, transparent 100%) !important;} }") //visited color WORKS, background-color doesn't. SlateBlue is ok.
