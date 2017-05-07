b018010234542fdd86a78edaf6520503b10664b5
MartR <juneyourtech@users.noreply.github.com>
2017-05-07 11:06:39 +0300

    Update Hub_rez.user.js
    Margin fix for custom footer msg.

--- Hub_rez.user.js
+++ Hub_rez.user.js
@@ -3,7 +3,7 @@
 // @namespace   hub_rez
 // @description Changes GitHub appearance for mobile. All trademarks are the property of their respective owners.
 // @updateURL https://github.com/juneyourtech/GM_Hub/raw/master/Hub_rez.user.js
-// @version     1.0.1
+// @version     1.0.1.1
 // @include     https://github.com/*
 // @include     https://*.github.com/*
 // @grant       GM_addStyle
@@ -26,7 +26,7 @@ GM_addStyle("DIV.file-actions {padding-left:84px;}") //
 //WIKI and SIDEBAR
 GM_addStyle("DIV.wiki-body .markdown-body {padding:0px 10px; margin:0px;}")
 
-GM_addStyle("@media only screen and (max-width:825px) { DIV.has-rightbar .wiki-body, DIV.has-rightbar .wiki-footer {margin-right:unset;}   DIV.wiki-rightbar {border:solid 1px Silver; margin-left:7px;} }")
+GM_addStyle("@media only screen and (max-width:825px) { DIV.has-rightbar .wiki-body, DIV.has-rightbar .wiki-footer {margin-right:unset;}   DIV.wiki-rightbar {border:solid 1px Silver; margin-left:7px;}  DIV.wiki-wrapper .wiki-footer .wiki-empty-box {margin:0px 0px;} }")
 
 GM_addStyle("@media only screen and (max-width:460px) { DIV.wiki-body .markdown-body {padding:0px 5px;}  DIV.wiki-rightbar {width:180px;} }")
 
